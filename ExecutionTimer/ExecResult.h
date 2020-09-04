363bdac0827991dfede271bb26ee8860bfc75fbf
Onur Şimşek <38193815+simsekonur@users.noreply.github.com>
2020-09-04 16:25:09 +0300

    Add files via upload

---
+++ ExecutionTimer/ExecResult.h
@@ -0,0 +1,7 @@
+typedef struct ExecResult{
+
+    char funcName[128];
+
+    long difference ;
+
+}myExecResult;
---
+++ ExecutionTimer/ExecTimer.h
@@ -0,0 +1,28 @@
+// header
+#ifndef ET
+#define ET
+#include<pthread.h>
+#include<mqueue.h>
+
+class ExecTimer{
+    private:
+		long beginTime;
+        pthread_t newThread; 
+        pthread_attr_t attr;
+		static mqd_t mq;
+		static size_t size;
+	
+		static const char* mqName;
+		static const char* threadName;
+		
+    public:
+//        ExecTimer();
+		~ExecTimer();
+        int StartUp();
+        int ShutDown();
+		void Begin();
+        void End(const char * fn);
+		void Wait();
+        static void* Run(void * arg);
+};
+#endif 
